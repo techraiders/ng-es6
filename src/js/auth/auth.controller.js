f0311787181e2cb98c0d1c2e9303bf296a1dd80f
navneet <hello_navneet@hotmail.com>
2017-07-27 06:19:17 +0530

    Creates registration and logging.

--- src/js/auth/auth.controller.js
+++ src/js/auth/auth.controller.js
@@ -1,7 +1,8 @@
 class AuthCtrl {
-  constructor($state) {
+  constructor(User, $state) {
     'ngInject';
 
+    this._User = User;
     this.title = $state.current.title;
     this.authType = $state.current.name.replace('app.', '');
   }
@@ -9,6 +10,19 @@ class AuthCtrl {
   submitForm() {
     this.submitting = true;
     console.log(this.formData);
+
+    this._User.attemptAuth(this.authType, this.formData).then(
+        // Callback for success
+        (res) => {
+          this.isSubmitting = false;
+          console.log(res);
+        },
+        // Callback for failure
+        (err) => {
+          this.isSubmitting = false;
+          console.log(err.data.errors);
+        }
+      );
   }
 }
 
