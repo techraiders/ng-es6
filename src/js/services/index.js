7e18174335181ded6ed163b7ce09bcefa84fb3e9
navneet <hello_navneet@hotmail.com>
2017-07-27 06:09:21 +0530

    Creates new service, User service.

--- src/js/services/index.js
+++ src/js/services/index.js
@@ -3,6 +3,9 @@ import angular from 'angular';
 // Create the module where our functionality can attach to
 let servicesModule = angular.module('app.services', []);
 
+// services
+import UserService from './user.service';
+servicesModule.service('User', UserService);
 
 
 export default servicesModule;
